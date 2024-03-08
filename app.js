a129ffb7fd7e43c9afc19d075e0bfd5c335af887
dipu kumar <dipukumardevcod@email.com>
2024-03-08 18:07:28 +0530

    complet the first page of the website

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-let page1Content = document.querySelector("#page1-content");
+function cursorEffect(){
+    let page1Content = document.querySelector("#page1-content");
 let cursor = document.querySelector("#cursor");
-page1Content.addEventListener("mousemove",function(dits){
-    cursor.style.left = dits.x+"px"
-    cursor.style.top = dits.y+"px"
+page1Content.addEventListener("mousemove", function (dits) {
+    // cursor.style.left = dits.x+"px"
+    // cursor.style.top = dits.y+"px"
+
+    //  code of gsap 
+    gsap.to("#cursor", {
+     x:dits.x,
+     y:dits.y
+    })
 })
+
+page1Content.addEventListener("mouseenter",function(){
+    gsap.to(cursor,{
+        scale:1,
+        opacity:1
+    })
+})
+page1Content.addEventListener("mouseleave",function(){
+    gsap.to(cursor,{
+        scale:0,
+        opacity:1
+    })
+})
+}
+cursorEffect();
